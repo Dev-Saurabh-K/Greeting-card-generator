d1519e16e443b35ff1b90bfc2dd87a02873c3010
Dev-Saurabh-K <saurabhkumar.sakr@gmail.com>
2025-11-30 21:42:03 +0530

    error handling

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-import { normal,
+import {
+  normal,
   elegantDarkLuxury,
   softGlassmorphism,
   retroPopBrutalist,
-  bohoArch, } from "./templates/img.template.js";
+  bohoArch,
+} from "./templates/img.template.js";
 
 import { htmlToImage } from "./controllers/img.controller.js";
 
 import readline from "readline-sync";
 
-const designs={
-    1:normal,
-    2:elegantDarkLuxury,
-    3:softGlassmorphism,
-    4:retroPopBrutalist,
-    5:bohoArch,
-}
+const designs = {
+  1: normal,
+  2: elegantDarkLuxury,
+  3: softGlassmorphism,
+  4: retroPopBrutalist,
+  5: bohoArch,
+};
 
 // console.log(designs[1]);
+while (true) {
+  let index = readline.question(
+    "-----Designs Available:-----\n1.normal\n2.nelegantDarkLuxury\n3.softGlassmorphism\n4.retroPopBrutalist\n5.bohoArch\n\n\nType Design Index to continue..."
+  );
+  if (designs[index]!=undefined) {
+    console.log(index, ":", designs[index]);
 
-let index = readline.question("-----Designs Available:-----\n1.normal\n2.nelegantDarkLuxury\n3.softGlassmorphism\n4.retroPopBrutalist\n5.bohoArch\n\n\nType Design Index to continue...");
-console.log(index,":",designs[index]);
-
-let name=readline.question("Enter your Name: ");
-console.log("Fill details about Bithday Boy...");
-let bname = readline.question("Enter Name: ");
-let dob=readline.question("Enter dd/mm/yyyy");
-
-
-
-htmlToImage(name,bname,dob,designs[index]);
+    let name = readline.question("Enter your Name: ");
+    console.log("Fill details about Bithday Boy...");
+    let bname = readline.question("Enter Name: ");
+    let dob = readline.question("Enter dd/mm/yyyy");
+    htmlToImage(name, bname, dob, designs[index]);
+    break;
+  } else {
+    console.log("Invalid Input! Try Again.....");
+  }
+}
